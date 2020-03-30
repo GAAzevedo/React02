332b0dd3ac5be0a7b11ca1b27f9250dde44e66d3
Benneffi3504 <guilhermeazevedo.3504@aluno.saojudas.br>
2020-03-30 20:57:17 -0300

    react03

---
+++ components/LembreteInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, TextInput, Button, StyleSheet} from 'react-native'
+const LembreteInput = (props) => {
+    return (
+    <View style={styles.lembreteView}>
+    {/* usuário irá inserir lembretes aqui*/}
+    <TextInput
+    placeholder="Lembrar..."
+    style={styles.lembreteInputText}
+    onChangeText={capturarLembrete}
+    value={lembrete}
+    />
+    <Button title="+" onPress={() => props.onAdicionarLembrete(lembrete)}/>
+    </View>
+    );
+}
+const styles = StyleSheet.create({
+    lembreteView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 8
+    },
+        lembreteInputText: {
+        width: '80%',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        padding: 2
+    }
+});
+export default LembreteInput;
